bab495e834c0aba068e720ea7d6ac4bbe3219a3d
Flor Marshall <flordmarshall@gmail.com>
2021-02-25 14:35:41 -0500

    add task props as object

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./styles/tailwind.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+ReactDOM.render(<App tasks={DATA} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
