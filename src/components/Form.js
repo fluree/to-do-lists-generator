36e401fa197ca0cc5e380ed6fec963cc863340d2
Flor Marshall <flordmarshall@gmail.com>
2021-03-01 21:31:46 -0500

    refactor to add ability to assign

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function Form(props) {
-  const [name, setName] = useState("");
+  const [state, setState] = useState({ task: '', assignee: '' });
 
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    props.addTask(name);
-    setName("");
-  }
+    props.addTask(state.task);
+    props.addName(state.assignee);
+    setState({ task: '', assignee: '' });
+    console.log(state.assignee);
+  };
 
-  function handleChange(e) {
-    setName(e.target.value);
-  }
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <h2 className="">
-        <label htmlFor="new-todo-input" className="">
+      <h2 className=''>
+        <label htmlFor='new-todo-input' className=''>
           Add a task and assign it to a person
         </label>
       </h2>
       <div>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="border-2 rounded-md border-gray-400"
-          name="text"
-          autoComplete="off"
-          value={name}
-          onChange={handleChange}
-        />
-        {/* <input
-          type="text"
-          id="new-name-input"
-          className="border-2 rounded-md border-gray-400"
-          name="text"
-          autoComplete="off"
-          value={name}
-          onChange={handleChange}
-        /> */}
+        <label>
+          Task: {''}
+          <input
+            type='text'
+            id='new-todo-input'
+            className='border-2 rounded-md border-gray-400'
+            name='task'
+            autoComplete='off'
+            value={state.task}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Assignee: {''}
+          <input
+            type='text'
+            id='new-assignee-input'
+            className='border-2 rounded-md border-gray-400'
+            name='assignee'
+            autoComplete='off'
+            value={state.assignee}
+            onChange={handleChange}
+          />
+        </label>
       </div>
-      <button type="submit" className="">
+      <button type='submit' className=''>
         Add
       </button>
     </form>
