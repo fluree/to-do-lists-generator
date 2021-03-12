51e6b697fd431b7fe53ea73bdffa5accf67a9f24
Flor Marshall <flordmarshall@gmail.com>
2021-03-12 16:58:37 -0500

    refactor App component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const FILTER_MAP = {
 const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks);
+  const [lists, setLists] = useState(props.data);
   const [filter, setFilter] = useState('All');
 
   const handleSubmit = (list) => {
@@ -23,47 +23,42 @@ function App(props) {
 
   //tasks are marked as complete or not
   function toggleTaskCompleted(id) {
-    const updatedTasks = tasks.map((task) => {
+    const updatedTasks = lists.map((task) => {
       if (id === task.id) {
         return { ...task, completed: !task.completed };
       }
       return task;
     });
-    setTasks(updatedTasks);
+    setLists(updatedTasks);
   }
   //tasks are deleted
   function deleteTask(id) {
-    const remainingTasks = tasks.filter((task) => id !== task.id);
-    setTasks(remainingTasks);
+    const remainingTasks = lists.filter((task) => id !== task.id);
+    setLists(remainingTasks);
   }
   //tasks are edited
   function editTask(id, newName) {
-    const editedTaskList = tasks.map((task) => {
+    const editedTaskList = lists.map((task) => {
       if (id === task.id) {
         return { ...task, task: newName };
       }
       return task;
     });
-    setTasks(editedTaskList);
+    setLists(editedTaskList);
   }
 
-  const taskList = tasks
-    .filter(FILTER_MAP[filter])
-    .map((task) => (
-      <Todo
-        name={task.name}
-        description={task.description}
-        id={task.id}
-        task={task.task}
-        completed={task.completed}
-        assignee={task.assignee}
-        email={task.email}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-        editTask={editTask}
-      />
-    ));
+  const taskList = lists.map((list) => (
+    <Todo
+      name={list.name}
+      description={list.description}
+      id={list.id}
+      tasks={list.tasks}
+      key={list.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
 
   const filterList = FILTER_NAMES.map((task) => (
     <FilterButton
@@ -74,17 +69,16 @@ function App(props) {
     />
   ));
 
-  function addList({ name, description, task, assignee, email }) {
+  function addList({ name, description, tasks }) {
     const newList = {
       name: name,
       description: description,
       id: 'todo' + nanoid(),
-      task: task,
-      completed: false,
-      assignee: assignee,
-      email: email,
+      tasks: tasks,
     };
-    setTasks([...tasks, newList]);
+    setLists((lists) => [...lists, newList]);
+    console.log(lists);
+    setLists('');
   }
 
   return (
