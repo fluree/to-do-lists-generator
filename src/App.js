a240c7f1d253abbcc1dd11b396a3a8c765a0826b
Flor Marshall <flordmarshall@gmail.com>
2021-04-05 13:21:22 -0400

    logo placement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './components/Form';
 import { Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import ListContainer from './components/ListContainer';
 import Blue_Brandmark from './Images/Blue_Brandmark.png';
 
@@ -8,12 +9,14 @@ function App() {
   return (
     <Grid container alignItems='center' justify='center'>
       <Grid item xs={8}>
-        <h1>TodoLists</h1>
-        <img
-          src={Blue_Brandmark}
-          alt='Fluree Yeti logo'
-          style={{ width: '48px', height: '48px' }}
-        />
+        <Box display='flex' flexDirection='row' justifyContent='space-evenly'>
+          <h1>TodoLists</h1>
+          <img
+            src={Blue_Brandmark}
+            alt='Fluree Yeti logo'
+            style={{ width: '48px', height: '48px' }}
+          />
+        </Box>
         <Form />
       </Grid>
       <Grid item xs={8}>
