acb5013a2ea8a720cb3be3533dbadbf885010fda
Flor Marshall <flordmarshall@gmail.com>
2021-04-05 10:30:17 -0400

    move Fluree logo tor import use

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Form from './components/Form';
 import { Grid } from '@material-ui/core';
 import ListContainer from './components/ListContainer';
+import Blue_Brandmark from './Images/Blue_Brandmark.png';
 
 function App() {
   return (
     <Grid container alignItems='center' justify='center'>
       <Grid item xs={8}>
         <h1>TodoLists</h1>
+        <img
+          src={Blue_Brandmark}
+          alt='Fluree Yeti logo'
+          style={{ width: '48px', height: '48px' }}
+        />
         <Form />
       </Grid>
       <Grid item xs={8}>
