3c60535cb794812e65ba1a38b292a4fcd69224f6
Flor Marshall <flordmarshall@gmail.com>
2021-04-20 14:29:54 -0400

    remove unnecessary index

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
 
-function InputComponent({ title, name, value, change, index }) {
+function InputComponent({ title, name, value, change }) {
   return (
     <TextField
       type='text'
