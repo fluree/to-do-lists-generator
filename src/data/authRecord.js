324e455c0f647d02a409d4f92f6afa0a7acdfe54
Flor Marshall <flordmarshall@gmail.com>
2021-05-18 13:12:15 -0400

    edits to smart functions

---
+++ src/data/authRecord.js
@@ -0,0 +1,37 @@
+export default {
+  Tucca: {
+    publicKey:
+      '02abe6276ba35ce73990f5e5e25564512454b12ad814c62c9cc0a1556879673d86',
+    privateKey:
+      '190ee2398f6ec5fd779517b571af038b9588e66cc5de9aba30f7b3478f68b452',
+    authId: 'Tf1F6w8Rx277tDkuYLz9k7okrdqaBvtneEs',
+  },
+  Isabel: {
+    publicKey:
+      '03c1607197f4904c1e79f5563596492c511373094b0a0f55dba6877184b280d542',
+    privateKey:
+      '36637d7af5255e75fbfadce07d5aaca08f3b8e990a2a11397f311ef73c4c8820',
+    authId: 'Tf3UqA6Mw1kWismnCwuCy2E6tM4n8rUMcN7',
+  },
+  Jane: {
+    publicKey:
+      '02d9e9d78937952b729b357a2cb682593cb136632d457abeeee53277700b8f9b6e',
+    privateKey:
+      'dc529a6d40fb096eb9bc26df7bca480b52e520e021a82a61afca787974ff31a0',
+    authId: 'Tf8Qh9ZnAJwojK6rwdmLcyBtAwdgiC6shjL',
+  },
+  Flor: {
+    publicKey:
+      '03c1607197f4904c1e79f5563596492c511373094b0a0f55dba6877184b280d542',
+    privateKey:
+      '9c1a81038f926adc89f9e67b2bb41052747d49e199e0f0043c9c481ecbb59d5f',
+    authId: 'TfDPiP3dMc53a95NnQE2TJq3Bt7EGfA2xbk',
+  },
+  Mannie: {
+    publicKey:
+      '0368e89ed07101e959407218ac6278118076a446646348783e7d4eebacc5e8c933',
+    privateKey:
+      '4b46f8fa717046d33867f2d20b06d5cd0b437710c5889f4fc85a39862c2f5f20',
+    authId: 'TfDmCkYnGTctGVSw8sDnrwpnKueqK63vBsn',
+  },
+};
