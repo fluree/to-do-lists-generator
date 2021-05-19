031453d27c1ece02110859b81ede78a33de6cfbb
Flor Marshall <flordmarshall@gmail.com>
2021-05-19 13:45:04 -0400

    adjust auth file

---
+++ src/data/usersAuth.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  Tucca: {
+    name: 'Tucca',
+    publicKey:
+      '02f3a83d60cb8baea378071ca22ce7792237727880477c5551d23f8f22ab227fd2',
+    privateKey:
+      '33862b77c042edde477c6ab80aa62a0b130a8290d4f16ca10a0bae137f90cbce',
+    authId: 'TfDk1nsChytKQJsEHf9TRUP9ic6K6fmZTnj',
+  },
+  Isabel: {
+    name: 'Isabel',
+    publicKey:
+      '03d7abdff6904fde26d5b8868cebd509074fe74cc9345ee755b6bcdb3f0bec8ec6',
+    privateKey:
+      '42d2f62e7901c101d61d7902ac40e3c4c94490fabab08307cb20a8615ff2de73',
+    authId: 'Tf6CBLZqx8sDQhar9vwiPU1hW4gFH41nhJT',
+  },
+  Jane: {
+    name: 'Jane',
+    publicKey:
+      '03e0ed4af0f48ec97043f4c6362ec748484a5f4e25a6a0b7285c0f05686deb229c',
+    privateKey:
+      '7565164d9f6349bac517ca38895dd3bac195fbff3b6daca84e77b5427e96e995',
+    authId: 'Tf88dnvpwFNczKkRwe2JhUoAabyBttKzDaV',
+  },
+  Flor: {
+    name: 'Flor',
+    publicKey:
+      '020dccea7dac46a1bf5a78f730f7581ee24f9abb1733093bca552c6655e3b6a6c7',
+    privateKey:
+      'db235518f4652812f9041700836b3c99cfa536d53c502f718a69cb6f2af05a2b',
+    authId: 'TfEBXM7Jv1m1cdfiFFwKBRA2dpGsy2E4TDP',
+  },
+  Mannie: {
+    name: 'Mannie',
+    publicKey:
+      '032e6d4370889c47a0180c0f058e0415778a59500bfb90d2d99e3b921180374bb0',
+    privateKey:
+      '4031ceb81a311554071646beb3c330d203d34cc49e193156c3cc5257372dd997',
+    authId: 'TfCYdrNBWbviwXEd88LWWTxWde95uJikL6v',
+  },
+};
